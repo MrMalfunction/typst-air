687c8dc1598e67a0d83925c08bc468c8fe704871
Amol Bohora <24508987+MrMalfunction@users.noreply.github.com>
2025-03-15 17:28:43 -0400

    Updated Sample Job

--- src/stores/jobs-store.js
+++ src/stores/jobs-store.js
@@ -7,7 +7,34 @@ export const useJobsStore = defineStore('jobsStore', {
         id: 1,
         title: 'Software Engineer',
         company: 'Tech Company',
-        description: 'Some description',
+        description: 'About the job\n' +
+          'Obsidian Security was founded in 2017 to solve the unaddressed blindspot of SaaS Security. SaaS applications provide the tools employees need to succeed and hold the business’ most critical information. If those tools become unavailable or that data is jeopardized, there is a detrimental impact on the organization.\n' +
+          '\n' +
+          'Obsidian proudly offers the industry\'s most comprehensive and powerful SaaS defense solution. We are committed to solving the challenge of SaaS Security for our customers as efficiently and effectively as possible.\n' +
+          '\n' +
+          'We’re a passionate team optimizing for impact by solving some of the biggest challenges in cybersecurity today. We listen closely to our customers, iterate quickly, and (over) deliver to delight them. Working at Obsidian means contributing to an industry-leading cybersecurity product in an environment where customer satisfaction, privacy, and data ethics are paramount.\n' +
+          '\n' +
+          'We are looking for a Software Engineer - Generalist to join our growing engineering team. As a generalist, you’ll be working across the entire stack to develop, deploy, and optimize cutting-edge security solutions. You will collaborate closely with product, security, and engineering teams to design, build, and maintain scalable and reliable software. This is an exciting opportunity for someone looking to make a direct impact in a fast-paced, dynamic environment.\n' +
+          '\n' +
+          'About The Role\n' +
+          '\n' +
+          'Full-Stack Development: Design, build, and maintain both front-end and back-end components for our SaaS security platform.\n' +
+          'Security Focus: Work on security-related features, integrating cutting-edge security protocols, and building tools that help secure our customers’ data and infrastructure.\n' +
+          'Collaboration: Work closely with other engineers, product managers, and designers to deliver high-quality features and enhance the platform\'s security.\n' +
+          'System Design: Participate in high-level system design and architecture discussions for new features and products.\n' +
+          'Problem Solving: Identify technical issues and implement solutions to improve performance, scalability, and security of the platform.\n' +
+          'Continuous Improvement: Drive continuous improvement in code quality, testing, and deployment processes.\n' +
+          'Innovation: Stay updated on security trends, technologies, and best practices, and contribute ideas to the development of new security features.\n' +
+          '\n' +
+          'Key Qualifications\n' +
+          '\n' +
+          '1-4 years of experience in software development experience.\n' +
+          'Experienced in React/ system engineering with a focus on data pipeline optimization.\n' +
+          'Solid understanding of distributed systems, databases, and performance tuning.\n' +
+          'Thorough understanding of React and its core principles.\n' +
+          'Experience with testing frameworks.\n' +
+          'Strong analytical skills and a data-driven approach to problem-solving.\n' +
+          'Excellent problem-solving skills, creativity, and the ability to work both independently and collaboratively.',
         dateAdded: Date.now(),
       },
     ],
