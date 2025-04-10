65b90c6bff91f3ba58afce93f3a49eb0f495f7ca
Amol Bohora <24508987+MrMalfunction@users.noreply.github.com>
2025-04-10 15:21:56 -0400

    Reverted AI Model

--- resume-ai/src/index.js
+++ resume-ai/src/index.js
@@ -82,7 +82,7 @@ export default {
 						'content-type': 'application/json',
 					},
 					body: JSON.stringify({
-						model: 'meta-llama/llama-4-scout-17b-16e-instruct',
+						model: 'llama3-70b-8192',
 						messages: ai_message_body,
 						temperature: 0.5,
 						max_completion_tokens: 1000,
