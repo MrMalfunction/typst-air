bc2d8ca32cea81afe59b28724c89008edfd34fc8
Amol Bohora <24508987+MrMalfunction@users.noreply.github.com>
2025-09-15 20:22:17 -0400

    Update Model

--- resume-ai/src/index.js
+++ resume-ai/src/index.js
@@ -73,7 +73,7 @@ async function handleImprovePoints(request, env) {
 				'content-type': 'application/json',
 			},
 			body: JSON.stringify({
-				model: 'llama3-70b-8192',
+				model: 'meta-llama/llama-4-scout-17b-16e-instruct',
 				messages: ai_message_body,
 				temperature: 0.5,
 				max_completion_tokens: 1000,
